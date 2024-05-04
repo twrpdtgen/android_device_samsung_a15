ceba77f0c55cdceab421da78198a96dd25ce624a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-04 05:04:06 +0200

    a15: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A155F Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_a15-user
+add_lunch_combo omni_a15-userdebug
+add_lunch_combo omni_a15-eng
